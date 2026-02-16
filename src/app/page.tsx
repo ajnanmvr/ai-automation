5ece30772db36ac32be76f3f4561cc9abedb3630
ajnanmvr <ajnanpulikkathody@gmail.com>
2026-02-16 23:10:12 +0530

    refactor: remove unused font imports and simplify body className

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import prisma from '@/lib/prisma'
 
 export default async function Home() {
   const users = await prisma.user.findMany();
+  const posts = await prisma.post.findMany({ include: { author: true } });
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
       <h1 className="text-4xl font-bold mb-8 font-(family-name:--font-geist-sans) text-[#333333]">
@@ -11,6 +12,11 @@ export default async function Home() {
         {users.map((user) => (
           <li key={user.id} className="mb-2">
             {user.name}
+          </li>
+        ))}
+        {posts.map((post) => (
+          <li key={post.id} className="mb-2">
+            {post.title + " by " + post.author.name + " (" + post.author.email + ")" + " - " + post.content}
           </li>
         ))}
       </ol>
