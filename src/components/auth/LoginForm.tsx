a27baa2e1710930fcda6926b30a3724cda9d9b18
ajnanmvr <ajnanpulikkathody@gmail.com>
2026-02-17 22:00:27 +0530

    feat: enhance LoginForm layout and functionality with improved structure and input components

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
 import { Button } from "../ui/button";
 import { useForm } from "react-hook-form";
+import { Input } from "../ui/input";
 
 export default function LoginForm() {
     const form = useForm({
@@ -13,40 +14,48 @@ export default function LoginForm() {
     })
 
     return (
-        <Card >
-            <CardHeader className="text-center">
+        <div className="flex items-center justify-center h-screen">
+            <Card className="max-w-3xl w-full">
+                <CardHeader className="text-center">
 
-                <CardTitle>Welcome back</CardTitle>
-                <CardDescription>Sign in to your account</CardDescription>
-            </CardHeader>
-            <Form {...form}>
-                <form>
-                    <div>
-                        <Button variant="outline" type="button">Login with Google</Button>
-                        <Button variant="outline" type="button">Login with GitHub</Button>
-                    </div>
-                    <div>
-                        <FormField name="email"
-                            render={({ field }) =>
-                                <FormItem>
-                                    <FormLabel>Email</FormLabel>
-                                    <FormControl>
-                                        <input type="email" placeholder="example@gmail.com" {...field} />
-                                    </FormControl>
-                                </FormItem>}
-                        />
-                        <FormField name="password"
-                            render={({ field }) =>
-                                <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <FormControl>
-                                        <input type="password" placeholder="••••••••" {...field} />
-                                    </FormControl>
-                                </FormItem>}
-                        />
-                    </div>
-                </form>
-            </Form>
-        </Card>
+                    <CardTitle>Welcome back</CardTitle>
+                    <CardDescription>Sign in to your account</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form>
+                            <div className="grid gap-6">
+
+                                <div className="flex flex-col gap-2">
+                                    <Button variant="outline" className="w-full" type="button">Login with Google</Button>
+                                    <Button variant="outline" className="w-full" type="button">Login with GitHub</Button>
+                                </div>
+
+                                <div className="grid gap-6">
+                                    <FormField name="email"
+                                        render={({ field }) =>
+                                            <FormItem>
+                                                <FormLabel>Email</FormLabel>
+                                                <FormControl>
+                                                    <Input type="email" placeholder="example@gmail.com" {...field} />
+                                                </FormControl>
+                                            </FormItem>}
+                                    />
+                                    <FormField name="password"
+                                        render={({ field }) =>
+                                            <FormItem>
+                                                <FormLabel>Password</FormLabel>
+                                                <FormControl>
+                                                    <Input type="password" placeholder="••••••••" {...field} />
+                                                </FormControl>
+                                            </FormItem>}
+                                    />
+                                </div>
+                            </div>
+                        </form>
+                    </Form>
+                </CardContent>
+            </Card>
+        </div>
     )
 }
