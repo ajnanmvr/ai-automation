a79b54cc2bb12f4a27dd570f717ebbed8b3ec7e1
ajnanmvr <ajnanpulikkathody@gmail.com>
2026-02-18 01:29:17 +0530

    feat: refactor authentication handling and enhance user retrieval logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { requireAuth } from "@/lib/auth-utils"
+import { caller } from "@/trpc/server"
 
 export default async function page() {
   await requireAuth()
 
+  const data = await caller.getUsers()
+
   return (
-    <div>page</div>
+    <div>{JSON.stringify(data)}</div>
   )
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,12 @@
 import prisma from "@/lib/prisma";
-import { baseProcedure, createTRPCRouter } from "../init";
+import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
 export const appRouter = createTRPCRouter({
-  getUsers: baseProcedure.query(() => {
-    return prisma.user.findMany();
+  getUsers: protectedProcedure.query(({ ctx }) => {
+    return prisma.user.findMany({
+      where: {
+        email: ctx.auth.user.email,
+      },
+    });
   }),
 });
 // export type definition of API
