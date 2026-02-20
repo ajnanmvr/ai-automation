7ae395b29af4ea399f530a6d5846554c78a9dcfe
ajnanmvr <ajnanpulikkathody@gmail.com>
2026-02-20 16:40:14 +0530

    feat: integrate OpenAI and Anthropic SDKs into AI execution function

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,6 +2,8 @@ import prisma from "@/lib/prisma";
 import { inngest } from "./client";
 import { generateText } from "ai";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import { createAnthropic } from "@ai-sdk/anthropic";
 
 export const helloWorld = inngest.createFunction(
   { id: "create-workflow" },
@@ -21,6 +23,8 @@ export const helloWorld = inngest.createFunction(
 );
 
 const google = createGoogleGenerativeAI();
+const anthropic = createAnthropic();
+const openAi = createOpenAI();
 
 export const executeAi = inngest.createFunction(
   { id: "execute-ai" },
@@ -28,11 +32,37 @@ export const executeAi = inngest.createFunction(
 
   async ({ event, step }) => {
     await step.sleep("pretend", "5s");
-    const { steps } = await step.ai.wrap("gemini-about-ajnan", generateText, {
-      model: google("gemini-2.5-flash"),
-      system: "you are a helpful assistant",
-      prompt: "who is muhammed ajnan p",
-    });
-    return steps;
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-about-ajnan",
+      generateText,
+      {
+        model: google("gemini-2.5-flash"),
+        system: "you are a helpful assistant",
+        prompt: "who is muhammed ajnan p",
+      },
+    );
+    const { steps: openAiSteps } = await step.ai.wrap(
+      "openai-about-ajnan",
+      generateText,
+      {
+        model: openAi("gpt-4.1-mini"),
+        system: "you are a helpful assistant",
+        prompt: "who is muhammed ajnan p",
+      },
+    );
+    const { steps: anthropicSteps } = await step.ai.wrap(
+      "anthropic-about-ajnan",
+      generateText,
+      {
+        model: anthropic("claude-3-7-sonnet-20250219"),
+        system: "you are a helpful assistant",
+        prompt: "who is muhammed ajnan p",
+      },
+    );
+    return {
+      geminiSteps,
+      openAiSteps,
+      anthropicSteps,
+    };
   },
 );
