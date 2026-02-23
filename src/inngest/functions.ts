5b9101b8723687459091228007aa16f69fbb2968
ajnanmvr <ajnanpulikkathody@gmail.com>
2026-02-23 16:12:16 +0530

    feat: refactor AI execution function and remove unused workflow creation logic

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,26 +1,9 @@
-import prisma from "@/lib/prisma";
-import { inngest } from "./client";
-import { generateText } from "ai";
+import { createAnthropic } from "@ai-sdk/anthropic";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { createOpenAI } from "@ai-sdk/openai";
-import { createAnthropic } from "@ai-sdk/anthropic";
+import { generateText } from "ai";
+import { inngest } from "./client";
 
-export const helloWorld = inngest.createFunction(
-  { id: "create-workflow" },
-  { event: "workflow/create.new" },
-  async ({ event, step }) => {
-    await step.sleep("step 1", "5s");
-    await step.sleep("step 2", "5s");
-
-    await step.run("create-new-workflow", () => {
-      return prisma.workFlows.create({
-        data: {
-          name: "New Workflow from bg",
-        },
-      });
-    });
-  },
-);
 
 const google = createGoogleGenerativeAI();
 const anthropic = createAnthropic();
