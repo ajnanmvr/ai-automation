d39591d29dcac62b2ae035afcd0e906ec58133ff
ajnanmvr <ajnanpulikkathody@gmail.com>
2026-02-23 20:23:08 +0530

    feat: add EntityHeader component for improved workflow UI

---
+++ src/components/EntityComponents.tsx
@@ -0,0 +1,54 @@
+import { PlusIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+type TEntityHeaderProps = {
+    title: string,
+    description?: string,
+    newButtonLabel?: string,
+    disabled?: boolean,
+    isCreating?: boolean
+} & (
+        | { onNew: () => void; newButtonHref?: never }
+        | { newButtonHref: string, onNew?: never }
+        | { onNew?: never; newButtonHref?: never }
+    )
+
+
+export default function EntityHeader({
+    title, description, disabled, isCreating, onNew, newButtonLabel, newButtonHref,
+}: TEntityHeaderProps) {
+    return (
+        <div className="flex flex-row items-center justify-between gap-x-4">
+            <div className="flex flex-col">
+                <h1 className="text-lg md:text-xl font-semibold">{title}</h1>
+                {description && (
+                    <p className="text-xs md:text-sm text-muted-foreground">
+                        {description}
+                    </p>
+                )}
+            </div>
+            {onNew && !newButtonHref && (
+                <Button
+                    disabled={disabled || isCreating}
+                    onClick={onNew}
+                    size="sm"
+                >
+                    <PlusIcon className="size-4" />
+                    {newButtonLabel}
+                </Button>
+            )}
+            {!onNew && newButtonHref && (
+                <Button
+                    asChild
+                    size="sm"
+                >
+                    <Link href={newButtonHref}>
+                        <PlusIcon className="size-4" />
+                        {newButtonLabel}
+                    </Link>
+                </Button>
+            )}
+        </div>
+    )
+}
