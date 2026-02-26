53db33943fb0ba53c3f959f3d47b679a9f120a9a
ajnanmvr <ajnanpulikkathody@gmail.com>
2026-02-26 07:02:26 +0530

    feat: integrate @xyflow/react for enhanced editor functionality with node and edge management

--- src/app/(dashboard)/(editor)/workflows/[workFlowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workFlowId]/page.tsx
@@ -19,7 +19,7 @@ export default async function CredentialsPage({ params }: IPageProps) {
       <ErrorBoundary fallback={<EditorError />}>
         <Suspense fallback={<EditorLoading />}>
           <EditorHeader workflowId={workflowId} />
-          <main>
+          <main className="flex-1">
             <Editor workflowId={workflowId} />
           </main>
         </Suspense>
